a8467ba35599cc140bdef145359fdaa39634463a
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-23 12:18:47 -0300

    Inclusão de texto

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 const HeroSection = () => {
   return( 
   <section>
-    <h1 className="text-white mb-4 text-6xl font-extrabold">
+    <div className="grid grid-cols-1 lg:grid-cols-12">
+   <div className="col-span-7 place-self-center">
+    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
       Hello, Dev
       </h1>
-    <p className="text-[#ADB7BE]">
+    <p className="text-[#ADB7BE] text-lg lg:text-xl">
+     
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure fugit numquam incidunt, repudiandae quis quas dignissimos.
     </p>
+    </div>
+    <div className="col-span-5"></div>
+    </div>
     </section>
   );
 };
