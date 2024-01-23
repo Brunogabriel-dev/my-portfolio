50123c7181fefb8e831ad3ec3c1c5a55e4ab7f16
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-23 11:37:04 -0300

    Inclusão de texto

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+      <HeroSection/>
     </main>
   );
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return( 
+  <section>
+    <h1 className="text-white mb-4 text-6xl font-extrabold">
+      Hello, Dev
+      </h1>
+    <p className="text-[#ADB7BE]">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure fugit numquam incidunt, repudiandae quis quas dignissimos.
+    </p>
+    </section>
+  );
+};
+
+export default HeroSection
