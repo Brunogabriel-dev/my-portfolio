fb5ca8fbcd634bfd4efb259c40689b9e2aea0df7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-23 23:02:35 -0300

    Inclusão de texto

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,17 +4,17 @@ import Image from 'next/image';
 const HeroSection = () => {
   return( 
   <section>
-    <div className="grid grid-cols-1 lg:grid-cols-12">
-   <div className="col-span-7 place-self-center">
-    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+    <div className="grid grid-cols-1 sm:grid-cols-12">
+   <div className="col-span-7 place-self-center text-center sm:text-left">
+    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
       Hello, Dev
       </h1>
-    <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure fugit numquam incidunt, repudiandae quis quas dignissimos.
     </p>
     <div>
-      <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
-      <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Download CV</button>
+      <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
+      <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Download CV</button>
     </div>
     </div>
     <div className="col-span-5 place-self-center mt-4 lg:mt-0">
