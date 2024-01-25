268f3b81e0b97b440632c191a232c8045d00ca32
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-24 21:55:39 -0300

    Inclusão de texto

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,7 +17,9 @@ const HeroSection = () => {
       <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
         Hire, Me
         </button>
-      <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3">Download CV</button>
+      <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3">
+        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+      </button>
     </div>
     </div>
     <div className="col-span-5 place-self-center mt-4 lg:mt-0">
