1a3d94686efa8af1c8a1aab2cd47ea0aca01c3f2
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-24 23:02:06 -0300

    Inclusão de texto

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+ "use client";
 import React from 'react'
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return( 
@@ -7,8 +9,25 @@ const HeroSection = () => {
     <div className="grid grid-cols-1 sm:grid-cols-12">
    <div className="col-span-7 place-self-center text-center sm:text-left">
     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-      <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm{""}</span>
-      Dev
+      <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, Dev{""}
+      </span>
+      <br></br>
+      <TypeAnimation
+sequence={[
+  'Dev',
+  1000, 
+  'Web Developer',
+  1000,
+  'Mobile Developer',
+  1000,
+  'UI/UX Designer',
+  1000
+]}
+wrapper="span"
+speed={50}
+repeat={Infinity}
+/>
+      
       </h1>
     <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure fugit numquam incidunt, repudiandae quis quas dignissimos.
@@ -17,7 +36,7 @@ const HeroSection = () => {
       <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
         Hire, Me
         </button>
-      <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3">
+      <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border mt-3">
         <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
       </button>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav><div>
+      <Link href="/">LOGO</Link>
+      </div></nav>
+  )
+}
+
+export default Navbar
