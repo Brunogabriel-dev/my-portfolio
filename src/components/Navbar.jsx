e7a3ae36c2488d28bed01a0708815685e8452b91
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-02-09 21:26:34 -0300

    Inclusão de texto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
         <Link href="/" className="text-5xl text-white font-semibold">
           LOGO
         </Link>
-        <div className="menu">
+        <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul>
-            <li><Link href={"#about"}>About</Link></li>
+            <li><Link href={"#about"} className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded-md:p-0 hover:text-white">About</Link></li>
           </ul>
         </div>
       </div>
